2195bc57403e5a9a8455c4a03f451bb412643d4e
robsonxd <robsonxd@gmail.com>
2021-03-15 03:41:54 -0300

    atualização da classe pessoa e funcionario para classes abstratas

--- bootcamp/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main() {
-    val robson = Pessoa(nome = "Robson Santos", cpf = "123.123.231-11")
-    println(robson.nome)
-    println(robson.cpf)
-}
