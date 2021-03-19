a69e1ab7cc7ba5bce561125b9ecd9ecdc38ef7e8
robsonxd <robsonxd@gmail.com>
2021-03-19 05:20:41 -0300

    aprendendo groupBy

--- bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -2,9 +2,9 @@ package one.digitalinnovation.collections
 
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     //coleção de funcionários é uma "list"
     val funcionarios = listOf(joao, pedro, maria)
@@ -15,15 +15,28 @@ fun main() {
     println("==================================")
     println("O Find de Maria")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("==================================")
+    println("sortedBy")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("==================================")
+    println("groupBy")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
     )
 {
     override fun toString(): String = """"
         Nome:   $nome
-        Salario: $salario
+        Salario:    $salario
     """.trimMargin()
 }
