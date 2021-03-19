789f8f4bc32ca57dd219612cab37f5c7c8b573e6
robsonxd <robsonxd@gmail.com>
2021-03-19 04:48:05 -0300

    aprendendo a manipular arrays com count, find e any

--- bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -14,4 +14,18 @@ fun main() {
     println("========================")
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("========================")
+    // Quantos salários estão entre 2000 e 5000?? nesse RANGE
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("========================")
+    // Para buscar valores específicos
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("========================")
+    // Vamos encontrar qualquer valor onde a expressão seja válida
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
 }
