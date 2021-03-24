ad1e51597db7e1eae7001d7c7e708f4c2edd29bd
robsonxd <robsonxd@gmail.com>
2021-03-24 05:19:35 -0300

    mutableMapOf

--- bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ bootcamp/Collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -29,13 +29,3 @@ fun main() {
 
 }
 
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-    )
-{
-    override fun toString(): String = """"
-     Nome:$nome, Salario:$salario
-    """.trimMargin()
-}
