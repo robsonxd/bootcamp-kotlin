964e1119b6d186cbf45f2dca22a005d95ded722c
robsonxd <robsonxd@gmail.com>
2021-04-22 01:23:42 -0300

    calculadora de IMC. criei a segunda tela. falta mover a função matemática da tela 1 para tela 2

--- bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/MainActivity.kt
+++ bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.robx.imcapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        calcularBTN.setOnClickListener {
+            val intent = Intent(this, Tela2::class.java)
+            startActivity(intent)
+
+        }
+        }
+
 }
--- bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/ResultActivity.kt
+++ bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/ResultActivity.kt
@@ -1,11 +1,38 @@
 package com.robx.imcapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        setListeners()
     }
+
+    private fun setListeners() {
+
+        calcularBTN.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+    private fun calcularIMC (peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            resultadoTXT.text = "Seu IMC é de: $imc"
+            calcularBTN.hideKeyboard()
+
+        }
+    }
+}
+private fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE)
+            as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
