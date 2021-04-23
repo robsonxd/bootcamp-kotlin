d2fc9d99f69f8ae05fda6e016beea1396ebc4932
robsonxd <robsonxd@gmail.com>
2021-04-22 22:04:05 -0300

    App de cálculo de imc com tela de calculo e tela de informações

--- bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/MainActivity.kt
+++ bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/MainActivity.kt
@@ -17,12 +17,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
 
-        calcularBTN.setOnClickListener {
+        infoBTN.setOnClickListener {
             val intent = Intent(this, Tela2::class.java)
             startActivity(intent)
 
+
         }
+    }
+        private fun setListeners() {
+
+            calcularBTN.setOnClickListener {
+                calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+            }
+
+        }
+        private fun calcularIMC (peso: String, altura: String) {
+            val peso = peso.toFloatOrNull()
+            val altura = altura.toFloatOrNull()
+            if (peso != null && altura != null) {
+                val imc = peso / (altura * altura)
+                resultadoTXT.text ="Seu Imc é $imc"
+                calcularBTN.hideKeyboard()
+
+            }
         }
 
+}
+private fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE)
+            as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
--- bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/Tela2.kt
+++ bootcamp/imcapplication/app/src/main/java/com/robx/imcapplication/Tela2.kt
@@ -1,11 +1,19 @@
 package com.robx.imcapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_tela2.*
 
 class Tela2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+
+        voltarBTN.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
